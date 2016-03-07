de31dfb4577fc55c4a7f66aaed6e6697abafb986
Paul <paulalexrees@gmail.com>
2016-03-07 16:27:28 +0000

    adds balance acquiring

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,16 @@
 class Oystercard
+  DEFAULT_BALANCE = 0
+
+
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  def get_balance
+    balance
+  end
+
+private
+  attr_reader :balance
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,7 @@ require 'oystercard'
 describe Oystercard do
 
 	it 'intial balance on oystercard should be 0' do
- 	expect(subject.balance).to eq 0
-end
+ 	  expect(subject.get_balance).to eq 0
+  end
 
 end
