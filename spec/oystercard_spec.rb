c8ac2354d742d1baffd94accba68a0d097482a28
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-08 10:39:48 +0000

    First spec and method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) {described_class.new}
+  describe '#balance' do
+    it 'checks that it has a balance' do
+      expect(oystercard.balance).to eq 0
+    end
+  end
 
 end
