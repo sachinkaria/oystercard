ca7ef0ebd82d88d88c98a08f90e70b12ae529f33
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-09 15:56:56 +0000

    updated feature test

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,2 +1,17 @@
-require_relative 'lib/station.rb'
-require_relative 'lib/oystercard.rb'
+# require_relative 'lib/station.rb'
+require_relative '../lib/oystercard.rb'
+require_relative '../lib/journey.rb'
+
+oys = Oystercard.new
+oys.top_up 15
+oys.touch_in("Euston") # station should be optional
+
+oys.touch_out("Highbury & Islington")
+
+p oys.journeys
+
+# station should be optional
+
+oys.touch_out("Highbury & Islington")
+
+p oys.journeys
