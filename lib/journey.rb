0f53b5e09b6bc368b5116ca0f94792ee1ae00f22
Jazzy <jasmina.gasper@gmail.com>
2016-03-09 15:56:52 +0000

    moved fare method to journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,6 +2,10 @@ class Journey
 
   attr_reader :entry_station, :exit_station
 
+  PENALTY_FARE = 6
+  MAX_VALUE = 90
+  MIN_VALUE = 1
+
   def initialize
     @entry_station = nil
     @exit_station = nil
@@ -19,5 +23,12 @@ class Journey
     journey_record = {:entry_station=> @entry_station, :exit_station => @exit_station}
   end
 
+  def fare
+    if (@journey.entry_station = nil && @journey.exit_station != nil) || (@journey.entry_station != nil && @journey.exit_station = nil)
+    PENALTY_FARE
+    else
+    MIN_VALUE
+    end
+  end
 
 end
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,7 +4,6 @@ require_relative 'journey'
 class Oystercard
   MAX_VALUE = 90
   MIN_VALUE = 1
-  PENALTY_FARE = 6
 
   attr_reader :balance, :journeys
 
@@ -23,23 +22,14 @@ class Oystercard
   def touch_in(station)
      insufficient_funds
      @journey.entry_st(station)
-
   end
 
   def touch_out(station)
-    deduct(MIN_VALUE)
     @journey.exit_st(station)
     record_journey
     @journey = Journey.new
   end
 
-  def fare
-    if (@journey.entry_station = nil && @journey.exit_station != nil) || (@journey.entry_station != nil && @journey.exit_station = nil)
-      PENALTY_FARE
-    else
-    MIN_VALUE
-    end
-  end
 
     private
     def within_limit(cash)
@@ -52,9 +42,9 @@ class Oystercard
       raise message if balance < MIN_VALUE
     end
 
-    def deduct(fare)
-      @balance -= fare
-    end
+    # def deduct(fare)
+    #   @balance -= fare
+    # end
 
     def record_journey
       @journeys << @journey.record_journey
